fe204237ade5614f4ff097653cf05835464d3457
Lonkal <kalon.cheong@gmail.com>
2014-04-02 19:47:29 -0400

    Servlet/HTML Templates
    Added servlet/html templates for structuring and start of
    implementation for registering and logging in.
    
    Also added basic forms.

---
+++ final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/LoginServlet.java
@@ -0,0 +1,11 @@
+package com.screaminggreen.sculptor;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class LoginServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Login Attempt with username:" + req.getParameter("username") + " pw: " + req.getParameter("password"));
+	}
+}

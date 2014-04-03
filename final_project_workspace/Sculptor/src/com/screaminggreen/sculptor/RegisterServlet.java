cf8578e248def191c4f3bd8f0560b7e36925c75c
Lonkal <kalon.cheong@gmail.com>
2014-04-03 10:23:33 -0400

    Change to POST method
    change servlet methods to POST

--- final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/RegisterServlet.java
+++ final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/RegisterServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class RegisterServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Trying to register");
 	}

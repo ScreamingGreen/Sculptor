2bb6bfde692563128caaa6f58941713e1cfe86b2
Lonkal <kalon.cheong@gmail.com>
2014-04-04 18:47:46 -0400

    Layout for register servlet
    Layout for register servlet!

--- final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/RegisterServlet.java
+++ final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/RegisterServlet.java
@@ -1,11 +1,27 @@
 package com.screaminggreen.sculptor;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class RegisterServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Trying to register");
+		
+		//Get email and password
+		String email = req.getParameter("registerEmail");
+		String password = req.getParameter("registerPassword");
+		
+		//Put this information in datastore
+		
+		//Try to insert data, see if email exists already
+		
+		//If so, just print error
+		PrintWriter out = resp.getWriter();
+		out.println("You have an account already");
+		
+		//Else, print success
+		out.println("Success!");
 	}
 }

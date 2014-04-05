7f955c77d3030f3ee6c5186779dc72a01bd4301d
Lonkal <kalon.cheong@gmail.com>
2014-04-04 20:47:14 -0400

    LoginServlet changes
    Added functionality to logging in. It works on authenticating but
    doesn’t send them to their homepage (none yet). Also on failure,
    redirect back to log in page with error = true as GET params.

--- final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/LoginServlet.java
+++ final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/LoginServlet.java
@@ -1,11 +1,44 @@
 package com.screaminggreen.sculptor;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.http.*;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Login Attempt with username:" + req.getParameter("username") + " pw: " + req.getParameter("password"));
+		PrintWriter out = resp.getWriter();
+	    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		
+		String user = req.getParameter("username");
+		String pass = req.getParameter("password");
+
+	    Filter userFilter = new FilterPredicate("email", FilterOperator.EQUAL, user);
+	    Filter passFilter = new FilterPredicate("password", FilterOperator.EQUAL, pass);
+	    
+	    Query query = new Query("User").setFilter(userFilter).setFilter(passFilter);
+	    List<Entity> users = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1));
+	    
+	    if(users.size() > 0) {
+	    	out.println("User/pass found!");
+	    	
+	    	//Send them to their homepage
+	    } else {
+	    	out.println("No user/pass found...");	    	
+	    	resp.sendRedirect("/loginpage.html?error=true");	    	
+	    }
 	}
 }

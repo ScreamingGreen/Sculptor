77ebd888a11f23d80e85abdc4279f9a69bb1d754
Lonkal <kalon.cheong@gmail.com>
2014-04-05 20:54:47 -0400

    Check nulls for registering
    Check nulls for registering, just in case front end fails to validate.

--- final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/RegisterServlet.java
+++ final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/RegisterServlet.java
@@ -22,12 +22,19 @@ public class RegisterServlet extends HttpServlet {
 		//Get Password
 		String password = req.getParameter("registerPass");
 		
+		//Ain't nobody want null fields, 
+		//if the frontend fails to validate, we have to do it just in-case...
+		
+		if(webId == null || webId.isEmpty() || email == null || email.isEmpty() 
+				|| password == null || password.isEmpty()) {
+			resp.sendRedirect("/registeruser.jsp?error=true");
+		}		
 		
 		if(Professor.getProfessor(webId)==null){			
 			
 			Professor.createOrUpdateProfessor(webId, email, password);
 			 
-			 resp.sendRedirect("/loginpage.jsp?success=true");			 
+			resp.sendRedirect("/loginpage.jsp?success=true");			 
 		}
 		else{
 			//Error 

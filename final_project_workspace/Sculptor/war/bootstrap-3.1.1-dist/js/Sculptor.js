d543b2ccc47cb0e8052cada29e30260f9d30fc34
allenfung00 <allenfung00@yahoo.com>
2014-04-08 12:30:15 -0400

    Added basic teacher page to edit course pages
    Added basic teacher page to edit course pages, added create page on
    index page for testing

--- final_project_workspace/Sculptor/war/bootstrap-3.1.1-dist/js/Sculptor.js
+++ final_project_workspace/Sculptor/war/bootstrap-3.1.1-dist/js/Sculptor.js
@@ -1,8 +1,48 @@
-function init() {
-$(document).ready(function(){	
-				$("#slider").easySlider({
-					auto: true,
-					continuous: true 
-				});
-			});
+
+/* Variable used for new tab IDs */
+var x = 10238412;
+ 
+/* Adds new tab and content */
+function addPage(){
+	var newTabString = '#' + (x+1);
+	
+	/* Adds new tab for left side tab-bar */
+	$("#tab-bar")
+		.append(
+			/* Basic attributes for left side tab-bar */
+			$('<li></li>').append(
+			$('<a>New Tab</a>')
+				.attr('href', newTabString)
+				.attr('data-toggle', 'tab')
+				.attr('id', x)
+				.attr('onClick', 'tabClicked()')
+				.addClass('newTab'))
+		);
+	x++;
+	
+	/* Adds content on right side */
+	$(".tab-content")
+		.append(
+			$('<div></div>')
+				.addClass('tab-pane')
+				.attr('id', x)
+				.append(
+					$('<h3> Title </h3><input type="text" class="form-control" placeholder="Assignments">')
+					)
+				.append(
+					$('<h3> Information </h3><textarea class="form-control" row="3"></textarea>')
+					)
+		);
+		
+	x++;
 }
+
+
+/* Dynamic editing of tab names
+function tabClicked() {
+	$(".newTab").click(function(event) {
+		$(this).text('');
+	});
+}
+*/
+

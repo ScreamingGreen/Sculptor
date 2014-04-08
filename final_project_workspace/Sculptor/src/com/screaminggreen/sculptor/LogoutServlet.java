da001c3e7ab3db4ff3ce279bf0f337f57587d9fe
Lonkal <kalon.cheong@gmail.com>
2014-04-08 17:52:36 -0400

    Proper way to invalidate session
    Proper way to invalidate session

--- final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/LogoutServlet.java
+++ final_project_workspace/Sculptor/src/com/screaminggreen/sculptor/LogoutServlet.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LogoutServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
 		//Clear the session map
-		req.getSession().invalidate();
+		HttpSession session = req.getSession(false);
+		
+		if(session != null) {
+			session.invalidate();
+		}
 		
 		//Return to home page now
 		resp.sendRedirect("/index.html");
@@ -21,7 +26,11 @@ public class LogoutServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
 		//Clear the session map
-		req.getSession().invalidate();
+		HttpSession session = req.getSession(false);
+		
+		if(session != null) {
+			session.invalidate();
+		}
 		
 		//Return to home page now
 		resp.sendRedirect("/index.html");

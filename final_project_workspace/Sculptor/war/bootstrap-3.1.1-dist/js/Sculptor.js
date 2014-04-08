30af419256f40f12b73453bd2710fbff6703dd19
allenfung00 <allenfung00@yahoo.com>
2014-04-08 16:55:20 -0400

    made home, syllabus, schedule unable to be removed
    made home, syllabus, schedule unable to be removed

--- final_project_workspace/Sculptor/war/bootstrap-3.1.1-dist/js/Sculptor.js
+++ final_project_workspace/Sculptor/war/bootstrap-3.1.1-dist/js/Sculptor.js
@@ -15,8 +15,8 @@ function addPage(){
 				.attr('href', newTabString)
 				.attr('data-toggle', 'tab')
 				.attr('id', x)
-				.attr('onClick', 'tabClicked()')
-				.addClass('newTab'))
+				.attr('onClick', 'tabClicked()'))
+				.addClass('newTab')
 		);
 	x++;
 	
@@ -25,6 +25,7 @@ function addPage(){
 		.append(
 			$('<div></div>')
 				.addClass('tab-pane')
+				.addClass('newContentPane')
 				.attr('id', x)
 				.append(
 					$('<h3> Title </h3><input type="text" class="form-control" placeholder="Assignments">')
@@ -39,7 +40,8 @@ function addPage(){
 
 /* Removes current active tab */
 function removePage(){
-	$(".active").remove();
+	$(".active.newTab").remove();
+	$(".active.newContentPane").remove();
 }
 
 /* Dynamic editing of tab names

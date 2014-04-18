d4c9a06ec085563a3d0f321f031b587d3fcfcd95
Lonkal <kalon.cheong@gmail.com>
2014-04-18 18:25:45 -0400

    Now Parser Skeletons and Form HTMLs

---
+++ final_project_workspace/Sculptor/src/com/screaminggreen/formprocessing/ScheduleFormParser.java
@@ -0,0 +1,13 @@
+package com.screaminggreen.formprocessing;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ScheduleFormParser implements FormParser {
+
+	@Override
+	public void parse(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ final_project_workspace/Sculptor/src/com/screaminggreen/formprocessing/SyllabusFormParser.java
@@ -0,0 +1,13 @@
+package com.screaminggreen.formprocessing;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SyllabusFormParser implements FormParser {
+
+	@Override
+	public void parse(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

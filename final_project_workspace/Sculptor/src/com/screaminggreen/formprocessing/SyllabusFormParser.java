c0870308ae3b57fe1b0f899a64e93c6a1a3e8ee5
Lonkal <kalon.cheong@gmail.com>
2014-04-19 23:48:58 -0400

    Backend implementation for Schedule/Syllabus
    Backend implementation for Schedule/Syllabus parsers. Need front end to
    fetch the data via JSON.

--- final_project_workspace/Sculptor/src/com/screaminggreen/formprocessing/SyllabusFormParser.java
+++ final_project_workspace/Sculptor/src/com/screaminggreen/formprocessing/SyllabusFormParser.java
@@ -2,12 +2,48 @@ package com.screaminggreen.formprocessing;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.appengine.api.datastore.Entity;
+import com.screaminggreen.beans.ProfessorBean;
+import com.screaminggreen.beans.SessionBean;
+import com.screaminggreen.datastore.CourseTab;
+import com.screaminggreen.datastore.DatastoreAPI;
+
 public class SyllabusFormParser implements FormParser {
 
+	public final static String TYPE_OF_COURSETAB = "Syllabus";
+	
 	@Override
 	public void parse(HttpServletRequest req) {
-		// TODO Auto-generated method stub
 
+		//Get the parameters
+		String description = req.getParameter("description");
+		String materials = req.getParameter("materials");
+		String infoAndHours = req.getParameter("infoAndHours");
+		String breakdown = req.getParameter("breakdown");
+		
+		//Get the webId
+		SessionBean sBean = (SessionBean) req.getSession().getAttribute("sessionBean");
+		if(sBean == null){
+			return;
+		}
+
+		ProfessorBean pBean = (ProfessorBean) sBean.getProfBean();		
+		if(pBean == null){
+			return;
+		}
+		
+		String webId = pBean.getWebId();
+		
+		Entity e = CourseTab.createOrGetCourseTab(webId, TYPE_OF_COURSETAB);
+		
+		//Set properties
+		e.setProperty("description", description);
+		e.setProperty("materials", materials);
+		e.setProperty("infoAndHours", infoAndHours);
+		e.setProperty("breakdown", breakdown);
+		
+		//Persist that shiet
+		DatastoreAPI.persistEntity(e);
 	}
 
 }

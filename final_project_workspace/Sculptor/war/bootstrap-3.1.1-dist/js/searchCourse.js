ad35605326226158c143236c02ee8f0e64f08012
Rahul Sarna <sarnarahul@gmail.com>
2014-04-29 23:49:47 -0400

    search course fixed, narrow down search according to text typed in

--- final_project_workspace/Sculptor/war/bootstrap-3.1.1-dist/js/searchCourse.js
+++ final_project_workspace/Sculptor/war/bootstrap-3.1.1-dist/js/searchCourse.js
@@ -1,7 +1,7 @@
 
 	  var availableTags = ["C++","C"]; 
 
-
+/*
 $(document).ready(function() {
 	
 	$(function(){
@@ -31,11 +31,45 @@ $(document).ready(function() {
         			        alert('Error displaying courses' + jsonData);
         			    }
         		  });
-        	}	        	
+        	},
+//        	minLength: 2
         });
 		
 		
 	});
+    
+});
+*/
+
+$(document).ready(function() {
+	
+	
+	$.ajax({
+	    type: 'post',
+	    url: '/searchCourse',
+	    dataType: 'text',
+	    success: function(jsonData) {
+	    	
+	    	var parsedJSON = jQuery.parseJSON(jsonData);
+	    	var data = parsedJSON.data[0];
+	    	
+	    	var data1 = [];
+	    	
+	    	for(var i=0;i<parsedJSON.data.length;i++)        			    	
+	    		data1.push(parsedJSON.data[i].name);
+	    	
+	    	$( "#search" ).autocomplete({
+
+	        	source: data1,
+	        	minLength: 0
+	        });
+	    	
+	    	
+	    },
+	    error: function(jsonData) {
+	        alert('Error displaying courses' + jsonData);
+	    }
+  });
     
 });
 
